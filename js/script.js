bf70b18d62460d04bb79147f8b7aaf8a0fd50ed7
Purdey Chambraud <purdey.chambraud@inwebo.com>
2019-08-17 22:29:28 +0200

    add score system, remove event listeners, modify cosmetic

--- js/script.js
+++ js/script.js
@@ -1,65 +1,114 @@
-const rock = document.querySelector(".js-rock");
-const paper = document.querySelector(".js-paper");
-const scissor = document.querySelector(".js-scissor");
-const message = document.querySelector(".js-message");
+const message = document.querySelector("#js-message");
+const messageEndGame = document.querySelector("#js-end-game-message");
+const playerScoreIHM = document.querySelector("#js-player-score");
+const computerScoreIHM = document.querySelector("#js-computer-score");
+const startButton = document.querySelector("#js-button-start-game");
+const gameBoard = document.querySelector("#js-game-board");
+const displayScoreToReach = document.querySelector("#js-score-to-reach");
+const scoreToReach = 8;
 
-var playerChoice;
-var computerChoice;
-var result;
 
-var computerChoice = Math.random();
+let playerScorePoint;
+let computerScorePoint;
+let isGameFinish;
 
-if (computerChoice < 0.34) {
-    computerChoice = "rock";
-} else if (computerChoice < 0.67) {
-    computerChoice = "paper";
-}  else {
-    computerChoice = "scissor";
+
+function initGame() {
+	playerScorePoint = 0;
+	computerScorePoint = 0;
+	displayScoreToReach.innerHTML=scoreToReach;
+	isGameFinish = false;
+	messageEndGame.innerHTML = "";
+	initIHM();
 }
 
-console.log(computerChoice);
+function playerHasChosen(playerChoice) {
+	gameTurn(playerChoice)
+}
 
-var rockSelected = rock.addEventListener('click', () => {
-	playerChoice = "rock";
-	if (computerChoice === playerChoice ) {
-		result ="No winner";
-	} else if (computerChoice === "paper" ) {
-		result ="You loose, paper covers the stone"
+function initIHM() {
+	gameBoard.style.display = "flex";
+	gameBoard.className = 'game-board game-board--displayed';
+	message.innerHTML = "Good Luck!";
+	startButton.innerHTML = "Restart";
+	playerScoreIHM.innerHTML = playerScorePoint;
+	computerScoreIHM.innerHTML = computerScorePoint;
+}
+
+function endGameIHM(isPlayerWinning) {
+	if (isPlayerWinning) {
+		messageEndGame.innerHTML = "Yeah! you win!";
 	} else {
-		result ="You win! rocks crushes sciscors!"
+		messageEndGame.innerHTML = "Oh no! Computer wins! :(";
 	}
-	message.innerHTML = result;
-	
-});
+}
 
-var paperSelected = paper.addEventListener('click', () => {
-	playerChoice = "paper";
-	if (computerChoice === playerChoice ) {
-		result ="No winner";
-	} else if (computerChoice === "rock") {
-		result ="You win, paper covers the stone";
+function gameTurn(playerChoice) {
+	if(isGameFinish) return;
+	let computerChoice = getComputerChoice();
+	compareChoices(playerChoice, computerChoice);
+}
+
+function compareChoices(playerChoice, computerChoice) {
+	let result;
+	if (playerChoice === "rock") {
+		if (computerChoice === playerChoice) {
+			result = "No winner";
+		} else if (computerChoice === "paper") {
+			result = "You loose, paper covers the rock"
+			computerScorePoint++;
+		} else {
+			result = "You win! rocks crushes sciscors!"
+			playerScorePoint++;
+		}
+	} else if (playerChoice === "paper") {
+		if (computerChoice === playerChoice) {
+			result = "No winner";
+		} else if (computerChoice === "rock") {
+			result = "You win, paper covers the rock";
+			playerScorePoint++;
+		} else {
+			result = "You loose! scissors cut the paper!";
+			computerScorePoint++;
+		}
 	} else {
-		result ="You loose! scissors cut the paper!";
+		if (computerChoice === playerChoice) {
+			result = "No winner";
+		} else if (computerChoice === "paper") {
+			result = "You Win, ciscors cut the paper";
+			playerScorePoint++;
+		} else {
+			result = "You loose! rock crushes scissors!";
+			computerScorePoint++;
+		}
 	}
-	message.innerHTML = result;
-	
-});
 
-var scissorSelected = scissor.addEventListener('click', () => {
-	playerChoice = "scissor";
-	if (computerChoice === playerChoice ) {
-		result = "No winner";
-	} else if (computerChoice === "paper" ) {
-		result ="You Win, ciscors cut the paper";
+	isGameFinish = playerScorePoint >= scoreToReach || computerScorePoint >= scoreToReach;
+	if (isGameFinish) {
+		message.innerHTML = `Final score : 
+		 you ${playerScorePoint} - ${computerScorePoint} computer `;
+		if (playerScorePoint > computerScorePoint) {
+			endGameIHM(true);
+		} else {
+			endGameIHM(false);
+		}
 	} else {
-		result ="You loose! rock crushes scissors!";
+		message.innerHTML = result;
 	}
-	message.innerHTML = result;
-	
-});
+	playerScoreIHM.innerHTML = playerScorePoint;
+	computerScoreIHM.innerHTML = computerScorePoint;
+}
 
 
-
-if (rockSelected) {
-	rock.style.backgroundColor = "orange";
+function getComputerChoice() {
+	let computerChoiceNum = Math.random();
+	let computerChoice;
+	if (computerChoiceNum < 0.34) {
+		computerChoice = "rock";
+	} else if (computerChoiceNum < 0.67) {
+		computerChoice = "paper";
+	} else {
+		computerChoice = "scissor";
+	}
+	return computerChoice;
 }
